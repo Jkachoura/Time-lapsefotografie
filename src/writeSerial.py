3fe26634eab9c8b7df76c9584ec0d42ad774f886
JKachoura <jaouadkachoura@hotmail.com>
2023-02-24 14:30:48 +0100

    updated so that theres no need to change the COM Port manually

--- src/writeSerial.py
+++ src/writeSerial.py
@@ -1,6 +1,31 @@
 # Test code for sending serial commands to ZOE, this command will reboot the ZOE cell imager.
 import serial
+import sys
 
-ser = serial.Serial('COM12', 115200, timeout=1)
+def serial_ports():
+    """ Lists serial port names
+
+        :raises EnvironmentError:
+            On unsupported or unknown platforms
+        :returns:
+            A list of the serial ports available on the system
+    """
+    if sys.platform.startswith('win'):
+        ports = ['COM%s' % (i + 1) for i in range(256)]
+    else:
+        raise EnvironmentError('Unsupported platform')
+
+    result = []
+    for port in ports:
+        try:
+            s = serial.Serial(port)
+            s.close()
+            result.append(port)
+        except (OSError, serial.SerialException):
+            pass
+    return result
+
+comPort = serial_ports()[0]
+ser = serial.Serial(comPort, 115200, timeout=1)
 
 ser.write(b'reboot\r\n')
