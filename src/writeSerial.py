1790858b60db41221c338e1c80ad05ab833e633d
tobiasdb-ai <tobiasdebildt@gmail.com>
2023-03-10 13:24:33 +0100

    Finished

--- src/writeSerial.py
+++ src/writeSerial.py
@@ -1,15 +1,16 @@
 # Test code for sending serial commands to ZOE, this command will reboot the ZOE cell imager.
 import serial
+import time
 import sys
 
 def serial_ports():
-    """ Lists serial port names
+    ''' Lists serial port names
 
         :raises EnvironmentError:
             On unsupported or unknown platforms
         :returns:
             A list of the serial ports available on the system
-    """
+    '''
     if sys.platform.startswith('win'):
         ports = ['COM%s' % (i + 1) for i in range(256)]
     else:
@@ -28,4 +29,21 @@ def serial_ports():
 comPort = serial_ports()[0]
 ser = serial.Serial(comPort, 115200, timeout=1)
 
-ser.write(b'reboot\r\n')
+capture = [1981,1973]
+
+
+
+def touchButt(input):
+    xString = f'sendevent /dev/input/event1 3 53 {str(input[0])}\r\n'
+    yString = f'sendevent /dev/input/event1 3 54 {str(input[1])}\r\n'
+
+    ser.write(b'sendevent /dev/input/event1 3 57 0\r\n')
+    ser.write(bytes(xString, encoding= 'utf-8'))
+    ser.write(bytes(yString, encoding= 'utf-8'))
+    ser.write(b'sendevent /dev/input/event1 0 0 0\r\n')
+    time.sleep(0.1)
+    ser.write(b'sendevent /dev/input/event1 3 57 -1\r\n')
+    ser.write(b'sendevent /dev/input/event1 0 2 0\r\n')
+    ser.write(b'sendevent /dev/input/event1 0 0 0\r\n')
+
+touchButt(capture)
