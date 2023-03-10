50f3cabbbe2e5a11f290c33c269281a702612bca
tobiasdb-ai <tobiasdebildt@gmail.com>
2023-03-10 13:47:25 +0100

    test kerstlampjes

--- src/writeSerial.py
+++ src/writeSerial.py
@@ -3,6 +3,14 @@ import serial
 import time
 import sys
 
+capture = [1981,1973]
+export = [1982,1974]
+live = [1941,435]
+wLed = [87, 762]
+bLed = [88, 1041]
+gLed = [89, 1251]
+rLed = [90, 1503]
+
 def serial_ports():
     ''' Lists serial port names
 
@@ -26,14 +34,6 @@ def serial_ports():
             pass
     return result
 
-if __name__ == "__main__":
-    comPort = serial_ports()[0]
-    ser = serial.Serial(comPort, 115200, timeout=1)
-
-capture = [1981,1973]
-
-
-
 def touchButt(input):
     xString = f'sendevent /dev/input/event1 3 53 {str(input[0])}\r\n'
     yString = f'sendevent /dev/input/event1 3 54 {str(input[1])}\r\n'
@@ -47,4 +47,15 @@ def touchButt(input):
     ser.write(b'sendevent /dev/input/event1 0 2 0\r\n')
     ser.write(b'sendevent /dev/input/event1 0 0 0\r\n')
 
-touchButt(capture)
+if __name__ == "__main__":
+    comPort = serial_ports()[0]
+    ser = serial.Serial(comPort, 115200, timeout=1)
+    while True:
+        touchButt(wLed)
+        time.sleep(3)
+        touchButt(bLed)
+        time.sleep(3)
+        touchButt(gLed)
+        time.sleep(3)
+        touchButt(rLed)
+        time.sleep(3)
