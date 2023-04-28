63131e4de848dd9a14de3338186d4368e74d8f7c
JKachoura <jaouadkachoura@hotmail.com>
2023-04-28 15:06:07 +0200

    esp32 can setup own wifi

---
+++ src/wifi.ino
@@ -0,0 +1,16 @@
+#include <WiFi.h>
+
+const char* ssid     = "ZOE";
+const char* password = "BML_010_AP";
+
+void setup()
+{
+    Serial.begin(115200);
+    Serial.println("\n[*] Creating AP");
+    WiFi.mode(WIFI_AP);
+    WiFi.softAP(ssid, password);
+    Serial.print("[+] AP Created with IP Gateway ");
+    Serial.println(WiFi.softAPIP());
+}
+
+void loop(){}
